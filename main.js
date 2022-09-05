645997713b47a90b0b67318b447f0f0b1cd99d9b
Reem <reembsrat@gmail.com>
2022-09-05 18:33:20 +0300

    event listener and input working

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-document.querySelectorAll('.leftPanel').addEventListener('click', putNumberInInput)
-const numbers = document.querySelector('.numbers');
+
+// const numbers = document.querySelectorAll('.numbers');
+const nums=document.querySelectorAll('.num')
+
+
 const input =document.querySelector('.input');
 
+// This makes it so every button can hear the click and runs a callback.In the callback,the number is taken as val and put into the input.
+
+nums.forEach(number=>{
+    number.addEventListener('click',(event)=>{
+        let val= number.innerHTML;
+        console.log(val)
+        input.innerHTML=val
+    })
+
+})
 
 
-function putNumberInInput() {
-    // const input =document.querySelector('.input');
 
-    let val = numbers.innerText;
-    alert(val)
-    
-    input.innerHTML=val
-
-    // -----------
-// this is onclick
-// -------------
-// --------------
-
-    
-}
