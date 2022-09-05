24bba73ccac18f0888870b845823f2a1c5cf9cfe
Reem <reembsrat@gmail.com>
2022-09-06 00:55:43 +0300

    working C btn

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
 
-// const numbers = document.querySelectorAll('.numbers');
 const nums=document.querySelectorAll('.num')
-
-
 const input =document.querySelector('.input');
+const operators=document.querySelectorAll('.operator');
+const clear=document.querySelector('.clear');
 
 // This makes it so every button can hear the click and runs a callback.In the callback,the number is taken as val and put into the input.
 
 nums.forEach(number=>{
     number.addEventListener('click',(event)=>{
+
         let val= number.innerHTML;
         console.log(val)
-        input.innerHTML=val
+        input.innerHTML+=val
     })
 
 })
+// The opertor btns can hear the click and run a callback. in the callback, the operator is taken as val and put into the input.
+
+operators.forEach(operator=>{
+    operator.addEventListener('click',()=>{
+        let val=operator.innerHTML;
+        input.innerHTML+=val
+
+
+    })
+})
+
+// 
+clear.addEventListener("click",emptyInput);
+function emptyInput(){
+    input.textContent="";
+}
 
 
 
