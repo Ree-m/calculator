fcbe4ad507c9c7344c9d173e02cb49cb3423e546
Reem <reembsrat@gmail.com>
2022-09-07 16:42:49 +0300

    working operators btn

--- main.js
+++ main.js
@@ -1,36 +1,53 @@
 
-const nums=document.querySelectorAll('.num')
-const input =document.querySelector('.input');
-const operators=document.querySelectorAll('.operator');
-const clear=document.querySelector('.clear');
+const nums = document.querySelectorAll('.num')
+const input = document.querySelector('.input');
+const operators = document.querySelectorAll('.operator');
+const clear = document.querySelector('.clear');
+const equalTo = document.querySelector('.equal');
+
 
 // This makes it so every button can hear the click and runs a callback.In the callback,the number is taken as val and put into the input.
 
-nums.forEach(number=>{
-    number.addEventListener('click',(event)=>{
+nums.forEach(number => {
+    number.addEventListener('click', (event) => {
 
-        let val= number.innerHTML;
+        let val = number.innerHTML;
         console.log(val)
-        input.innerHTML+=val
+        input.innerHTML += val
     })
 
 })
 // The opertor btns can hear the click and run a callback. in the callback, the operator is taken as val and put into the input.
 
-operators.forEach(operator=>{
+operators.forEach(operator => {
     operator.addEventListener('click',()=>{
-        let val=operator.innerHTML;
-        input.innerHTML+=val
+        let operatorVal = operator.innerHTML;
+        let inputContent=input.innerHTML
+        
+        console.log(operatorVal)
+        
+//making it so that i cant type an operator after an operator. Like,i cant type ++ or +-* 
 
-
-    })
+        if(inputContent[inputContent.length-1]=="+" || inputContent[inputContent.length-1]=="-" || inputContent[inputContent.length-1]=="*"  || inputContent[inputContent.length-1] =="/" ){
+         return input.innerHTML=input.innerHTML
+        }
+         input.innerHTML+=operatorVal
+            
+ })
 })
 
-// 
-clear.addEventListener("click",emptyInput);
-function emptyInput(){
-    input.textContent="";
+
+
+//C btn working, Makes the input empty on click.
+clear.addEventListener("click", emptyInput);
+function emptyInput() {
+    input.textContent = "";
 }
+// To solve the operations in the input.
+equalTo.addEventListener("click", runCal)
+function runCal() {
+    let inputCalc=eval(input.innerHTML);
+    input.innerHTML=inputCalc;
+    return inputCalc;
 
-
-
+}
